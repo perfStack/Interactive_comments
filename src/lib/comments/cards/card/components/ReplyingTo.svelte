e6035eb5405192555b41889bfb754d911d6fba25
perfStack <95343750+perfStack@users.noreply.github.com>
2022-07-04 16:21:15 +0530

    Added ReplyingTo into src/lib/comments/cards/card/components.

---
+++ src/lib/comments/cards/card/components/ReplyingTo.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import type { BaseCommentType, ReplyCommentType } from '../../../../data/data-store_types';
+  import { getContext } from 'svelte';
+  import { thisPostDataContextKey } from '../../../Scripts/Comments-contenxt';
+
+  const commentData: ReplyCommentType | BaseCommentType = getContext(thisPostDataContextKey);
+  const replyingTo = 'replyingTo' in commentData ? commentData.replyingTo : null;
+</script>
+
+{#if replyingTo}
+  <p class="replying-to-cont">
+    Replying to <span class="replying-to">@{replyingTo}</span>
+  </p>
+{/if}
+
+<style lang="scss">
+  .replying-to-cont {
+    color: var(--clr-nutrl-grayBlue);
+    font-size: 1.5rem;
+    margin-bottom: 0.3rem;
+  }
+
+  .replying-to {
+    color: var(--clr-pri-moderateBlue);
+    cursor: default;
+    font-weight: 600;
+  }
+</style>
